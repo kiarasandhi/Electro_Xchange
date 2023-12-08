53ceceb31afd6def2490d723284c2a7da4d9788f
The Neo Sufism <neosfm3@gmail.com>
2023-12-08 15:38:25 +0700

    login and register

--- app/src/main/java/com/example/electroxchange/MainActivity.java
+++ app/src/main/java/com/example/electroxchange/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.electroxchange;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
+public class MainActivity extends AppCompatActivity {
+    FirebaseAuth auth;
+    FirebaseUser user;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        auth = FirebaseAuth.getInstance();
+        user = auth.getCurrentUser();
+        if (user == null){
+            Intent intent = new Intent(getApplicationContext(), Login.class);
+            startActivity(intent);
+            finish();
+        }
     }
 }
--- app/src/main/java/com/example/electroxchange/Register.java
+++ app/src/main/java/com/example/electroxchange/Register.java
@@ -1,14 +1,74 @@
 package com.example.electroxchange;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Register extends AppCompatActivity {
-
+    EditText editEmail, editPass;
+    Button bRegis, bLogin;
+    FirebaseAuth auth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        auth = FirebaseAuth.getInstance();
+        editEmail = findViewById(R.id.editTextTextEmailAddress2);
+        editPass = findViewById(R.id.editTextTextPassword2);
+        bRegis = findViewById(R.id.button3);
+        bLogin = findViewById(R.id.button4);
+
+        bLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Login.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        bRegis.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email, pass;
+                email = editEmail.getText().toString();
+                pass = editPass.getText().toString();
+
+                if (TextUtils.isEmpty(email)) {
+                    Toast.makeText(Register.this, "Enter Email", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                if (TextUtils.isEmpty(pass)) {
+                    Toast.makeText(Register.this, "Enter Password", Toast.LENGTH_LONG).show();
+                    return;
+                }
+
+                auth.createUserWithEmailAndPassword(email, pass)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(Register.this, "Register Success.",
+                                            Toast.LENGTH_SHORT).show();
+                                } else {
+                                    Toast.makeText(Register.this, "Register Failed.",
+                                            Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+            }
+        });
     }
 }
