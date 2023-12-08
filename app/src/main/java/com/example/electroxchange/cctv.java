648658431b8bdf7d7d7f2e50ad24b93bca985425
Kiarasandhi <kiarasandhii@gmail.com>
2023-12-09 01:02:56 +0700

    Electro Xchange add banyak

---
+++ app/src/main/java/com/example/electroxchange/cctv.java
@@ -0,0 +1,14 @@
+package com.example.electroxchange;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class cctv extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cctv);
+    }
+}
