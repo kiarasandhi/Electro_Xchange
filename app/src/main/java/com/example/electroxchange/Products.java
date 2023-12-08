d47e2e59b53bacd7508ba8303d9147f58290c6e4
Kiarasandhi <kiarasandhii@gmail.com>
2023-12-09 01:13:26 +0700

    Electro Xchange revisi

--- app/src/main/java/com/example/electroxchange/Products.java
+++ app/src/main/java/com/example/electroxchange/Products.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class ChangePassword extends AppCompatActivity {
+public class Products extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_change_password);
+        setContentView(R.layout.activity_products);
     }
 }
